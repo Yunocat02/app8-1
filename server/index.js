2f9cd71ba2700b736b41c081fc5d33d3529e24a7
Chawanwit Kaenkong <s6303051623080@email.kmutnb.ac.th>
2023-11-21 19:00:06 +0700

    fix for 0.5 point QwQ

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 8000
-
-app.get('/',(request, response) => {
-    response.send(`
-    <!doctype html>
-    <html>
-    <head>
-        <title>Express.js</title>
-    </head>
-    <body>
-        <h3>Welcome to Express.js</h3>
-        <b>Express.js Fast, unopinionated, minimalist <br>
-        web framework for Node.js</b>
-    </body>
-    </html>
-    `)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+app.post('/api/form-post', (req, res) => {
+    let name = req.body.name || '';
+    let email = req.body.email || '';
+    let message = req.body.message || '';
+    let text = `
+        <table border="1">
+            <caption>ข้อมูลที่ส่งขึ้นไป</caption>
+            <tr><td>ชื่อ :</td><td>${name}</td></tr>
+            <tr><td>อีเมล :</td><td>${email}</td></tr>
+            <tr><td>ข้อความ :</td><td>${message}</td></tr>
+        </table>
+    `;
+    res.send(text);
+});
+app.listen(port, () => {
+    console.log('Server listening on port ' + port)
 })
-app.listen(port, () => console.log('Server listening on port'+port))
